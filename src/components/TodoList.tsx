f678b85c2df662a265956dcaf7b8610e0341b93e
Halina Utkina <utkina.ga@gmail.com>
2023-09-21 00:34:53 +0200

    task

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,6 @@
 import React, { ChangeEvent, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import style from "./TodoList.module.css";
 
 interface ITask {
     name: string;
@@ -8,49 +10,68 @@ interface ITask {
 const TodoList: React.FC = (): JSX.Element => {
     const [taskList, setTaskList] = useState<ITask[]>([
         {
-            name: "Погулятьсобакой",
+            name: "Погулять c собакой",
             completed: false,
         },
     ]);
     const [newTaskName, setNewTaskName] = useState<string>("");
 
     const handleIsDone = (index: number, checked: boolean) => {
-        setTaskList(prevList => prevList.map((task, i) => 
-            i === index ? {...task, completed: checked} : task
-        ));
+        setTaskList((prevList) =>
+            prevList.map((task, i) =>
+                i === index ? { ...task, completed: checked } : task
+            )
+        );
     };
 
-    const handleRemoveTask = (index: number):void => {
-        setTaskList(prevList => prevList.filter((_, i) => i !== index));
+    const handleRemoveTask = (index: number): void => {
+        setTaskList((prevList) => prevList.filter((_, i) => i !== index));
     };
 
     const handleAddTask = () => {
-        setTaskList(prevList => [...prevList, {name: newTaskName, completed: false}]);
+        setTaskList((prevList) => [
+            ...prevList,
+            { name: newTaskName, completed: false },
+        ]);
         setNewTaskName("");
     };
 
     return (
-        <div className="container">
-            <h1>Todo List App</h1>
-            <input 
-                type="text" 
-                value={newTaskName} 
-                onChange={(e) => setNewTaskName(e.target.value)} 
+        <div className={style.container}>
+            <h1 className={style.h1}>Todo List App</h1>
+            <input
+            className={style.input}
+                type="text"
+                value={newTaskName}
+                onChange={(e) => setNewTaskName(e.target.value)}
             />
-            <button onClick={handleAddTask}>Add Task</button>
+            <button
+                type="button"
+                className={`${style.button} alert alert-primary`}
+                onClick={handleAddTask}
+            >
+                Add Task
+            </button>
             <ol>
                 {taskList.map((task, index) => (
-                    <div key={index} className="task">
-                        <li>
+                    <div key={index} className={style.task}>
+                        <li className={style.li}>
                             <span>{index + 1}. </span>
                             {task.name}
                             <input
                                 type="checkbox"
                                 id="isDone"
                                 checked={task.completed}
-                                onChange={(e) => handleIsDone(index, e.target.checked)}
+                                onChange={(e) =>
+                                    handleIsDone(index, e.target.checked)
+                                }
                             />
-                            <button onClick={() => handleRemoveTask(index)}>Remove</button>
+                            <button
+                                className="alert alert-danger"
+                                onClick={() => handleRemoveTask(index)}
+                            >
+                                Remove
+                            </button>
                         </li>
                     </div>
                 ))}
